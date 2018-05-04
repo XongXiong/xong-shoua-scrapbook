c98f9a443664e0c7d202615cc86289ba60ed5147
Xong Xiong <xxiong3153@gmail.com>
2018-05-03 23:52:21 -0500

    added gallery nav and route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,18 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial']);
 
+myApp.config(function ($routeProvider, $locationProvider) { 
+    $locationProvider.hashPrefix('');
+    console.log('myApp -- config')
+    $routeProvider
+        .when('/home', {
+            templateUrl: '/views/templates/home.html',
+            controller: 'PostController as pc',
+        })
+        .when('/gallery', {
+            templateUrl: '/views/templates/gallery.html',
+            controller: 'PostController as pc',
+        })
+    .otherwise({
+        redirectTo: 'home'
+    });
+});
